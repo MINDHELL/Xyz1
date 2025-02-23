a1f2217db731e200933fcb9db2b75df8ad34e09f
MINDHELL <Tgpurpose640@gmail.com>
2025-02-23 13:01:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 # ✅ Environment Variables
 API_ID = int(os.getenv("API_ID", "27788368"))
-API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081"))
+API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7725707727:AAFtx6Sy-q6GgB9eaPoN2-oYPx2D6hjnc1g")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1002492623985"))
