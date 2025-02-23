2ae22a36524439577149df4bbe6096134c08448a
MINDHELL <Tgpurpose640@gmail.com>
2025-02-23 11:18:51 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,6 @@
+API_ID = "27788368"  # Your Telegram API ID
+API_HASH = "9df7e9ef3d7e4145270045e5e43e1081"  # Your Telegram API Hash
+BOT_TOKEN = "7725707727:AAFtx6Sy-q6GgB9eaPoN2-oYPx2D6hjnc1g"  # Your Telegram Bot Token
+MONGO_URL = "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Your MongoDB connection URL
+CHANNEL_ID = "-1002492623985"  # Your Telegram channel ID (must start with -100)
+OWNER_ID = "6860316927"  # Your Telegram User ID (only you can use /index)
