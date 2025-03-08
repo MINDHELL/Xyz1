d9ba927914f7b8f9a1fbab9ee8ea6dcafb6e5a16
MINDHELL <Tgpurpose640@gmail.com>
2025-03-08 23:15:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ bot = Client(
 )
 
 bot.run()
+
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!", 200
+
+def run_web():
+    app.run(host="0.0.0.0", port=8080)
+
+threading.Thread(target=run_web, daemon=True).start()
