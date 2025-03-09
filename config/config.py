f9cd7d8a769ba4112952bbce17c54c2d45fb5d1a
MINDHELL <Tgpurpose640@gmail.com>
2025-03-09 09:44:44 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,12 +1,12 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8064879322:AAH4Uv8ZJbHfDZRBnre_Uf4D-ew-Q8SCinc")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8064879322:AAFF_Y6RLji-hmwKxzvmZaHnV0FrZ1aPuK4")
 API_ID = int(os.getenv("API_ID", "27788368"))
 API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "6860316927"))  # Your Telegram User ID
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "2490575006")
+FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "-1002490575006")
 AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "20"))  # Time in seconds
 
 # Token Verification
