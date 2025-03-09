03223b342e6296e9996741dc45a348a59e5da2de
MINDHELL <Tgpurpose640@gmail.com>
2025-03-09 10:00:16 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8064879322:AAHvYtmZRsRamwHqhgUbXW-yZ5rjHhwdE4A")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7888029778:AAHeC7P5zONGjN3mY5q0Rm6-V1zzPx1ywEQ")
 API_ID = int(os.getenv("API_ID", "27788368"))
 API_HASH = os.getenv("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
